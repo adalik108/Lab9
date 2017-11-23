38446ac867d08ed28473aa579d573f116fa03c4e
Aaron Dalik <aaron.dalik@ucalgary.ca>
2017-11-23 15:09:18 -0700

    Initial Commit

---
+++ Lab9/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab9
+//
+//  Created by Aaron Dalik on 2017-11-23.
+//  Copyright © 2017 Aaron Dalik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
